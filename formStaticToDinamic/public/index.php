f08c2717df9a47c1b3d22a24f5770fc9fc8f888c
SimonDiLeoGIT <saimondiley@gmail.com>
2023-04-18 15:25:47 -0300

    Se agregan logd

--- formStaticToDinamic/public/index.php
+++ formStaticToDinamic/public/index.php
@@ -4,7 +4,14 @@
 
     use Paw\App\Controllers\PageController;
     use Paw\App\Controllers\ErrorController;
+    use Monolog\Logger;
+    use Monolog\Handler\StreamHandler;
+
+    $log = new Logger('mvc-app');
+    $log->pushHandler(new StreamHandler(__DIR__ . '/../logs/app.log', Logger::DEBUG));
+
     
+
     $whoops = new \Whoops\Run;
     $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
     $whoops->register();
@@ -12,33 +19,40 @@
     /*throw new \Exception("Error pibe");*/
 
 
+
     $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
+    $log->info("Petición a: {$path}");
+    
     $controller = new PageController;
 
     switch ($path) {
         case '/':
             $controller->index();
+            $log->info("Respuesta exitosa: 200");
             break;
         case '/services':
             $controller->services();
+            $log->info("Respuesta exitosa: 200");
             break;
             case '/about':
                 $controller->about();
+                $log->info("Respuesta exitosa: 200");
                 break;
         default:
                 $controller = new ErrorController;
                 $controller->notFound();
+                $log->info("Path not Found: 404");
             break;
     }
 
 
-/*echo "<pre>";
-var_dump($menu);
-die;*/
+    /*echo "<pre>";
+    var_dump($menu);
+    die;*/
 
-/*echo "<pre>";
-var_dump($_SERVER);
-die;*/
+    /*echo "<pre>";
+    var_dump($_SERVER);
+    die;*/
 
 ?>
